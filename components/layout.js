6ecf69db463544617e10029571bcf7ecb8abe0cf
Julian Tran <juliantran003@gmail.com>
2024-01-23 23:34:35 +0000

    added layout metadata

--- components/layout.js
+++ components/layout.js
@@ -6,10 +6,31 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-      />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        <title>Julian Tran</title>
+        <meta
+          name="description"
+          content="Songs & scores
+      "
+          key="desc"
+        />
+        <meta property="og:title" content={"Julian Tran"} />
+        <meta
+          property="og:description"
+          content="Songs & scores
+      "
+        />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta
+          name="keywords"
+          content="julian, tran, artist, composer, songwriting, music, score, film"
+        />
+      </Head>
+
       <main>{children}</main>
     </>
   );
