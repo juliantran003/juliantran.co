d021cf38171b5c3ce5f482d5ac40eb19da128a54
Julian Tran <juliantran003@gmail.com>
2024-01-24 12:59:02 +0000

    added more favicon metadata

--- components/layout.js
+++ components/layout.js
@@ -14,20 +14,31 @@ export default function Layout({ children }) {
         <title>Julian Tran</title>
         <meta
           name="description"
-          content="Songs & scores
-      "
+          content="My work explores the relationship between songwriting, soundscapes and mixed media scores. Most of it is written using a mixture of physical and digital instruments."
           key="desc"
         />
         <meta property="og:title" content={"Julian Tran"} />
         <meta
           property="og:description"
-          content="Songs & scores
-      "
+          content="My work explores the relationship between songwriting, soundscapes and mixed media scores. Most of it is written using a mixture of physical and digital instruments."
         />
         <meta property="og:image" content="/favicon.ico" />
         <meta
           name="keywords"
           content="julian, tran, artist, composer, songwriting, music, score, film"
+        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
         />
       </Head>
 
